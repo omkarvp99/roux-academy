249ed11f831acef0b3c9292b98a06be63a3fd048
omkar <omkvp@outlook.com>
2020-08-30 22:20:04 +0530

    First commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get('/', async (request, response, next) => {
+    try {
+      const artwork = await speakersService.getAllArtwork();
+      const topSpeakers = await speakersService.getList();
+      return response.render('layout', {
+        pageTitle: 'Welcome',
+        template: 'index',
+        topSpeakers,
+        artwork,
+      });
+    } catch (err) {
+      return next(err);
+    }
+    // if (!request.session.visitcount) {
+    //   request.session.visitcount = 0;
+    // }
+    // request.session.visitcount += 1;
+    // console.log(`Number of visits: ${request.session.visitcount}`);
+  });
+
+  // mount those routers
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+
+  return router;
+};
